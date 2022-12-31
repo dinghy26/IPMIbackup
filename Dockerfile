a748020f16f512a549b8881ea53f06f89d7101f9
Brian Miranda <dinghy26@gmail.com>
2022-12-31 16:38:50 +0300

    test some lines/add cron to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,21 @@ RUN apt upgrade -y
 RUN apt install python3 -y
 RUN apt install python3-pip -y
 RUN apt install ipmitool -y
+RUN apt install nano -y
 RUN pip install proxmoxmanager
 RUN pip install pyYAML
+RUN apt install cron -y
+
+
+# Add the cron job
+RUN crontab -l | { cat; echo "6 0 * * * bash /usr/app/src/dellStartBackup.py"; } | crontab -
 
 WORKDIR /usr/app/src
 
 COPY  dellStartBackup.py ./
 COPY  dellSetUp.yaml ./
+
+# run cron
+CMD cron
+
 
--- dellStartBackup.py
+++ dellStartBackup.py
@@ -33,7 +33,9 @@ SERVER_IP = setup_data.get('database').get('serverIP')
 IPMI_USERNAME = setup_data.get('IPMI').get('username')
 IPMI_PASSWORD = setup_data.get('IPMI').get('password')
 
-NOW = datetime.now()
+NOW = datetime.now().time() #time of the day
+HOURSTR = str(NOW)[:2] # first 2 number of the hour 
+
 IPMI_LOGIN = ("ipmitool -I lanplus -H %s -U %s -P %s ") % (SERVER_IP,IPMI_USERNAME, IPMI_PASSWORD)
 #proxmox_manager = ProxmoxManager(host = "%s:8006", user= "root@pam", token_name = "Tname", token_value = "Secret_value") % (SERVER_IP)
 
@@ -41,19 +43,25 @@ IPMI_LOGIN = ("ipmitool -I lanplus -H %s -U %s -P %s ") % (SERVER_IP,IPMI_USERNA
 bellow this text is the lines for testing change if need.
 
 """
-IPMI_GET_DATA = "sdr entity 7"
-IPMI_GET_TEMP = "sdr type Temperature"
+# IPMI_GET_DATA = "sdr entity 7"
+# IPMI_GET_TEMP = "sdr type#  Temperature"
+IPMI_GET_STATUS = "chassis status"
+IPMI_POWER_ON = "power on"
 
-temp_collection_command = IPMI_LOGIN + IPMI_GET_TEMP
-general_ipmi_data = IPMI_LOGIN + IPMI_GET_DATA
+# temp_collection_command = IPMI_LOGIN + IPMI_GET_TEMP
+# general_ipmi_data = IPMI_LOGIN + IPMI_GET_DATA
+get_status = IPMI_LOGIN + IPMI_GET_STATUS
 
-temp_data = os.popen(temp_collection_command).read().split()
-all_data = os.popen(general_ipmi_data).read().split()
+# temp_data = os.popen(temp_collection_command).read().split()
+# all_data = os.popen(general_ipmi_data).read().split()
+split_status = os.popen(get_status).read()#.split()
+# SERVER_TEMP = str(temp_data)
+#SERVER_DATA = str (all_data)
 
-SERVER_TEMP = str(temp_data)
-SERVER_DATA = str (all_data)
+# print (str(SERVER_TEMP))
+#print (SERVER_DATA)
+print(str(split_status))
 
-print (str(SERVER_TEMP))
-print (SERVER_DATA)
-
-
+#this line start the server
+#os.system(IPMI_LOGIN+IPMI_POWER_ON)
+print (HOURSTR) #this line gets de first 2 numbers of the time to use in the if statement
